12cc8169f929d4b640116ea67b9d69c4ff8c2fca
elijahc <ejcordov@ucsc.edu>
2017-11-24 19:42:26 -0800

    finished encode.

--- lab5/cipher.c
+++ lab5/cipher.c
@@ -21,7 +21,8 @@ void encode(char *key, char *cleartext, char *ciphertext)
 	int i, lk = strlen(key), lc = strlen(cleartext);
 	for (i = 0; i < lc; i+= 1) 
 	{
-		ciphertext[i] = (unsigned char) (cleartext[i] + key[i % lk]); // % ASCII
+		ciphertext[i] = (unsigned char) (cleartext[i] + key[i % lk])%ASCII;
+		printf ("%d\n",ciphertext[i]);
 	}
 	ciphertext[lc] = '\0';
 	return;
@@ -41,7 +42,7 @@ void decode(char *key, char *ciphertext, char *cleartext)
 	int i, lk = strlen(key), lc = strlen(ciphertext);
 	for (i = 0; i < lc; i+= 1) 
 	{
-		cleartext[i] = (unsigned char) (ciphertext[i] - key[i % lk]); // %ASCII
+		cleartext[i] = (unsigned char) (ciphertext[i] - key[i % lk]) % ASCII;
 	}
 	cleartext[lc] = '\0';
 	return;
@@ -49,7 +50,7 @@ void decode(char *key, char *ciphertext, char *cleartext)
 
 int main(void)
 {
-	char *cleartext = "Wearenotamused";
+	char *cleartext = "We are not amused";
 	char key[] = "01234";
 	char ciphertext[100];
 	printf("The given key is: %s\n",key);
